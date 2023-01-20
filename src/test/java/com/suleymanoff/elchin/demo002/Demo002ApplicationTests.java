ec824d65534e6d0720c35ce0f45244c72febd145
kheephora <elchin.suleymanoff@gmail.com>
2023-01-19 23:04:08 -0500

    first commit

---
+++ src/test/java/com/suleymanoff/elchin/demo002/Demo002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suleymanoff.elchin.demo002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
